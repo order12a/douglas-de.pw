62d693a84354926650741e02221f26bb72a6a7c9
order12a <uroshliev.volodymyr@gmail.com>
2024-03-02 19:53:47 +0200

    Extracted hooks as reusable actions

---
+++ tests/hooks/index.ts
@@ -0,0 +1,30 @@
+import { type Page, test } from '@playwright/test';
+import { Application } from '../../src/app/application';
+import { parfumSection } from '../../src/constants/menu-sections';
+
+export let page: Page;
+export let app: Application;
+
+export const setUp = () => {
+    test.beforeAll(async ({ browser }) => {
+        page = await browser.newPage();
+        app = new Application(page);
+        await app.landingPage.open();
+        await app.landingPage.expectLoaded();
+        await app.landingPage.acceptAllCookies();
+        await app.landingPage.navigateTo(parfumSection);
+        await app.parfumPage.expectLoaded();
+    });
+};
+
+export const tearDown = () => {
+    test.afterAll('Close page', async () => {
+        await page?.close();
+    })
+}
+
+export const clearFilters = () => {
+    test.afterEach('Clear filters', async () => {
+        await app?.parfumPage.clearAllFilters();
+    })
+}
--- tests/parfum-select.with-hooks.spec.ts
+++ tests/parfum-select.with-hooks.spec.ts
@@ -1,46 +1,54 @@
-import { test, type Page } from '@playwright/test';
-import { Application } from '../src/app/application';
+import { test } from '@playwright/test';
 import { scenarios } from './test-data/scenarios';
-import { parfumSection } from '../src/constants/menu-sections';
+import { app, setUp, tearDown, clearFilters } from './hooks';
 
 test.describe.configure({ mode: 'serial' });
 
-let page: Page;
-let app: Application;
+
+test.describe('Parfum Page tests', async () => {
+    setUp();
+    tearDown();
+    clearFilters();
+
+    for (const scenario of scenarios) {
+        test(`Select Parfum product to match ${scenario.expectedSpecialCategory}`, async () => {
+            for (const filter of scenario.criteria) {
+                await app.parfumPage.productSelect.expectLoaded();
+                await app.parfumPage.productSelect.clickSelectByCriteria(filter.filterType);
+                await app.parfumPage.productSelect.selectByCriteriaOption(filter.filterValue);
+                await app.parfumPage.productSelect.dismissDropdown();
+            }
+
+            await app.parfumPage.expectEyeCatcherProductsAppear(scenario.expectedSpecialCategory);
+        });
+    }
+})
+
+
+
 
 /**
  * Version Reuse single page between tests
  * We sacrifice test isolation to increase speed of the test run
  */
 
-test.beforeAll(async ({ browser }) => {
-    page = await browser.newPage();
-    app = new Application(page);
-    await app.landingPage.open();
-    await app.landingPage.expectLoaded();
-    await app.landingPage.acceptAllCookies();
-    await app.landingPage.navigateTo(parfumSection);
-    await app.parfumPage.expectLoaded();
-});
+// test.beforeAll(async ({ browser }) => {
+//     page = await browser.newPage();
+//     app = new Application(page);
+//     await app.landingPage.open();
+//     await app.landingPage.expectLoaded();
+//     await app.landingPage.acceptAllCookies();
+//     await app.landingPage.navigateTo(parfumSection);
+//     await app.parfumPage.expectLoaded();
+// });
   
-test.afterAll(async () => {
-    await page.close();
-});
+// test.afterAll(async () => {
+//     await page.close();
+// });
 
-test.afterEach('Clear selected filters', async() => {
-    await app.parfumPage.clearAllFilters();
-});
+// test.afterEach('Clear selected filters', async() => {
+//     await app.parfumPage.clearAllFilters();
+// });
 
-for (const scenario of scenarios) {
-    test(`Select Parfum product to match ${scenario.expectedSpecialCategory}`, async () => {
-        for (const filter of scenario.criteria) {
-            await app.parfumPage.productSelect.expectLoaded();
-            await app.parfumPage.productSelect.clickSelectByCriteria(filter.filterType);
-            await app.parfumPage.productSelect.selectByCriteriaOption(filter.filterValue);
-            await app.parfumPage.productSelect.dismissDropdown();
-        }
 
-        await app.parfumPage.expectEyeCatcherProductsAppear(scenario.expectedSpecialCategory);
-    });
-}
 
